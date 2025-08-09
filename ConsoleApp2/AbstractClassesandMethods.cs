9a2af221509c60da794993aa296c3940b22f7af8
kmmartey <kmmartey@st.ug.edu.gh>
2025-08-09 00:09:57 +0000

    circle

--- ConsoleApp2/AbstractClassesandMethods.cs
+++ ConsoleApp2/AbstractClassesandMethods.cs
@@ -5,3 +5,19 @@ abstract class Shape
 {
     public abstract double GetArea();
 }
+
+
+class Circle : Shape 
+{
+    public double Radius { get ;set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
