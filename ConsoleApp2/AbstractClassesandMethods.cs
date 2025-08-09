f28570c33bb622455d212e7c94ff5c7064f1749e
kmmartey <kmmartey@st.ug.edu.gh>
2025-08-09 00:11:18 +0000

    abstract class Rectangle

--- ConsoleApp2/AbstractClassesandMethods.cs
+++ ConsoleApp2/AbstractClassesandMethods.cs
@@ -21,3 +21,20 @@ class Circle : Shape
         return Math.PI * Radius * Radius;
     }
 }
+
+class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+}
